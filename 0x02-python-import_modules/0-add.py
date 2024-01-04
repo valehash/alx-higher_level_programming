c24deb7f4bbb819348840bae51d8405419113897
valehash <newlechim@gmail.com>
2024-01-04 21:26:30 +0100

    should work

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import add_0.add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
-sum = add(a, b)
+    a = 1
+    b = 2
+    sum = add(a, b)
 
 print(f"{a} + {b} = {sum}")
