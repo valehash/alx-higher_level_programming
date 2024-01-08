4e8416b3499b023a7c80a8a53914c96c3b5788fe
valehash <newlechim@gmail.com>
2024-01-08 14:03:56 +0100

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    lenght =  len(my_list)
+    lenght =  len(my_list)- 1
     if idx < 0 or idx > lenght:
         return my_list
     else:
