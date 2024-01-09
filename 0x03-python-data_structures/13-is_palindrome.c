d2f1854190dab59ea20d69e43cb970d2c276ce8d
valehash <newlechim@gmail.com>
2024-01-09 17:49:16 +0100

    abeg 13-linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -13,8 +13,9 @@ int is_palindrome(listint_t **head)
 	while (curr != second || curr != past)
 	{
 		second = curr;
-		while (second->next != past)
-			second = second->next;
+		/*while (second->next != past)*/
+			/*second = second->next;*/
+		second = second_hf(second, past);
 
 		if (curr->n != second->n)
 			return (0);
@@ -27,4 +28,19 @@ int is_palindrome(listint_t **head)
 	}
 
 	return (1);
+}
+
+/**
+ * second_hf - helper function to return a pointer to the last element in list
+ * @second_p: Pointer to starting point.
+ * @past_p: Pointer to stopping point.
+ *
+ * Return: Pointer to current stopping point.
+ */
+listint_t *second_hf(listint_t *second_p, listint_t *past_p)
+{
+	while (second_p->next != past_p)
+		second_p = second_p->next;
+
+	return second_p;
 }
