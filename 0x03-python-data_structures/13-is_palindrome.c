f329a1ff7204bbab49e04b3114f42ac174c94305
valehash <newlechim@gmail.com>
2024-01-09 16:51:15 +0100

    abeg 13-linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,3 +1,11 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: The start of the linked list.
+ *
+ * Return: If is palindrome, 1. Otherwise, 0.
+ */
 int is_palindrome(listint_t **head)
 {
 	listint_t *curr = *head, *past = NULL, *second = NULL;
