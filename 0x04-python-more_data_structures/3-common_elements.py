ee40e5324ee7eb7637fd5ccca7f3dc00f8b8d173
valehash <newlechim@gmail.com>
2024-01-10 16:33:56 +0100

    edgecase fixed?

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        if i in set_2:
-            return i
+    common_set = set()
+    for i in set_2:
+            if i in set_1:
+                common_set.add(i)
+    return common_set
