af5b184e5675b542d6633ae9f4d0a87071559e0d
valehash <newlechim@gmail.com>
2024-01-10 16:16:15 +0100

    added pybang

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-q    length = len(my_list)
+    length = len(my_list)
     copied_list = []
     for x in range(length):
         copied_list.append(my_list[x])
