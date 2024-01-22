d62332f7854365036b0db2fad7d13aaa6dc60433
valehash <newlechim@gmail.com>
2024-01-22 14:36:44 +0100

    fixed the return value

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    num = 0
     for k in range(x):
         try:
             print("{}".format(my_list[k]), end = "")
+            num = num + 1
         except IndexError:
-            k -= 1
             break
     print()
-    return k + 1
+    return num
