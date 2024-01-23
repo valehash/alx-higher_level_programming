e4421c44420c2cb534004de9710b00a4855b15d4
valehash <newlechim@gmail.com>
2024-01-23 16:54:29 +0100

    setter and getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a class """
+
+
+class Square:
+    """A class with a private instance atribute"""
+    pass
+
+    def __init__(self, size=0):
+        """Initializeing with a private instance of size"""
+        self.size = size
+
+    @property
+    def size(self):
+        """ Getter """
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """ Setter """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    
+    def area(self):
+        area =  self.__size**2
+        return area
