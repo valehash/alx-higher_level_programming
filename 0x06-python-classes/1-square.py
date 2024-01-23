4eac2d64f924d233fe32abf60b19e62b97d07ec0
valehash <newlechim@gmail.com>
2024-01-23 15:24:47 +0100

    woop

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a class """
+
+
+class Square:
+    """A class with a private instance atribute"""
+    pass
+
+    def __init__(self, __size):
+        """Initializeing with a private instance of size"""
+        self.__size = __size
+        
