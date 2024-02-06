c8c4491df7cb3efd71bee99036c5a6deae0feaed
valehash <newlechim@gmail.com>
2024-02-06 14:22:51 +0100

    1-main.py

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -2,11 +2,11 @@
 MyList = __import__('1-my_list').MyList
 
 my_list = MyList()
-my_list.append('c')
-my_list.append('d')
-my_list.append('a')
-my_list.append('v')
-my_list.append('x')
+my_list.append(6)
+my_list.append(5)
+my_list.append(7)
+my_list.append(88)
+my_list.append(77)
 print(my_list)
 my_list.print_sorted()
 print(my_list)
