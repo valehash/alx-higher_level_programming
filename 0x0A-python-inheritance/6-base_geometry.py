e2dc29b6ccb187fdbd93b864d805bb290ce79ece
valehash <newlechim@gmail.com>
2024-02-06 18:03:20 +0100

    guillme i learnt how to pronounce your name

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a geometry class with an area function that has not been implemented"""
+
+
+class BaseGeometry:
+    """a Base geometry class with a single public method"""
+
+    def area(self):
+        """A method to find the area of a shape"""
+        raise Exception("area() is not implemented")
