8c27f35093246ec3924bb7b37a4b73fff1ac6327
valehash <newlechim@gmail.com>
2024-02-06 14:39:42 +0100

    documented added 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""a function that returns if the object is from the class"""
+
 
 def is_same_class(obj, a_class):
+    """
+    Parameters:
+    -----------
+    obj:
+        obj is the object that is compared to the class
+    a_class:
+        a_class is the class comaprsaed against the object 
+    """
     if isinstance(obj,a_class):
         return True
     elif not isinstance(obj,a_class):
