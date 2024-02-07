4750ae8a3a7f48d31d60411f3be715f5a5e703eb
valehash <newlechim@gmail.com>
2024-02-07 16:55:12 +0100

    utf-8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
+"""scrpt with a function to open a file and print the output"""
+
+
 def read_file(filename=""):
     """a Fuction to open a file and print out the output"""
-    with open(filename,'r',ecncoding = 'utf-8') as file:
+    with open(filename,'r',encoding = 'utf-8') as file:
         for line in file:
             print(line, end='')
