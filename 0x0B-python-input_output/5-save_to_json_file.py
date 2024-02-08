d5fb1bef8f5f2310170b538613404ddc195961e1
valehash <newlechim@gmail.com>
2024-02-08 15:30:34 +0100

    5-main.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -16,5 +16,5 @@ def save_to_json_file(my_obj, filename):
     -----
     """
     to_append = json.JSONEncoder().encode(my_obj)
-    with open(filename, 'w', encoding='utf-8') as file:
+    with open(filename, 'a', encoding='utf-8') as file:
         file.write(to_append)
