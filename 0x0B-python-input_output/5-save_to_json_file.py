e48dba769b7a3d190a42d1963e7aeff212c25d9b
valehash <newlechim@gmail.com>
2024-02-08 13:37:18 +0100

    string saved as json

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""scrpt with a function to open a file and print the output"""
+
+
+import json
+def save_to_json_file(my_obj, filename):
+    """A Fuction to open a file and print out the output
+
+    Parameters:
+    -----------
+    my_obj:
+        The python file being converted to json string
+    returns
+    --------
+        Json string
+    -----
+    """
+    to_append = json.dumps(my_obj)
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(to_append)
