c610ef0b7411bb257dadbf3ff8e47687a4cd6340
valehash <newlechim@gmail.com>
2024-02-08 12:00:28 +0100

    PYCODE

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,13 +2,13 @@
 """scrpt with a function to open a file and print the output"""
 
 
-def append_write(filename="",text=""):
+def append_write(filename="", text=""):
     """A Fuction to open a file and print out the output
-    
+
     Parameters:
     -----------
     filename:
-        filename holds the name of the file you want to append to 
+        filename holds the name of the file you want to append to
     text:
         the text to be appended to the file
     retruns
@@ -16,7 +16,7 @@ def append_write(filename="",text=""):
         The lenght if what was appended
     -----
     """
-    with open(filename,'a', encoding = 'utf-8') as file:
+    with open(filename, 'a', encoding = 'utf-8') as file:
         file.write("{}".format(text))
 
-    return(len(text))
+    return (len(text))
