17c338274e700370b91f31d1e557e0f8305cb1f0
valehash <newlechim@gmail.com>
2024-02-13 14:09:31 +0100

    am i blind?

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,4 +98,4 @@ class Rectangle(Base):
                 print()
     
     def __str__(self):
-        return "[Recatangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,self.__y,self.__width,self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,self.__y,self.__width,self.__height)
