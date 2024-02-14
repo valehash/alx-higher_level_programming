ca1dd1b7738c241ea277ef282acbff88f55825a1
valehash <newlechim@gmail.com>
2024-02-14 15:31:56 +0100

    10-main.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,12 +7,32 @@ class Square(Rectangle):
     """The sqaure class for the aplication, which inherits from the rectangle class"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+
+
+    @property
+    def size(self):
+        """returning the value of the width"""
+        return self.width 
         
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        
+
+
+
 
 
     def __str__(self):
         """function to update the class by changing the return of the function"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,self.y,self.width)
+
+    
 
     """def update(self, *args, **kwargs):
         function to update the values passed into the class
