0beda0f418abe1b9dfc9ab7b449613a7ab069bab
vale <newlechim@gmail.com>
2024-03-13 14:53:34 +0100

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 -- displays the description of the table first table from the database hbtn_0c_0
-SHOW FIELDS from first_table;
+SELECT column_name, data_type, character_maximum_length, is_nullable, column_default
+FROM information_schema.columns
+where table_name = 'first_table';
