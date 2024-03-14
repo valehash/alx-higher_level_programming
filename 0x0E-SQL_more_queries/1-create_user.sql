44a00885e08a7d3ce4f6da0bdd7123d8e9f56da5
vale <newlechim@gmail.com>
2024-03-14 12:32:38 +0100

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- sql code to create a user with a passowrd
-CREATE USER  IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
