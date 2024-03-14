89dac1f95a21e164b822fde9e3202cd1e0c45723
vale <newlechim@gmail.com>
2024-03-14 12:36:56 +0100

    granted priviledges to user 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- sql code to create a user with a passowrd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- grants the user all priviledges
+GRANT ALL PRIVILEDGES ON *.* to 'user_0d_1'@'localhost'; 
