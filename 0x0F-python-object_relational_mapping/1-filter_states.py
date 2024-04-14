2aff959f5d55706814985a41917aed2df413ff8f
vale-hash <newlechim@gmail.com>
2024-04-14 08:56:37 +0100

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A module that reads the states startting with N from a db"""
+
+import sys
+import MySQLdb
+
+
+def connection(username, password, db_name):
+    """The function that connects to the db"""
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8")
+
+    curr = conn.cursor()
+
+    curr.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    query_rows = curr.fetchall()
+
+    for rows in query_rows:
+        print(rows)
+
+    curr.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    connection(sys.argv[1], sys.argv[2], sys.argv[3])
