917bc889bc38d0272c9adc9dbacc928ae840dac0
vale-hash <newlechim@gmail.com>
2024-04-14 16:02:51 +0100

    2-my filter done

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A module that reads the state in a db using user input"""
+
+import sys
+import MySQLdb
+
+
+def connection(username, password, db_name, searched_name):
+    """The function that connects to the db"""
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8")
+
+    curr = conn.cursor()
+
+    curr.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(searched_name))
+
+    query_rows = curr.fetchall()
+
+    for rows in query_rows:
+        print(rows)
+
+    curr.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    connection(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
