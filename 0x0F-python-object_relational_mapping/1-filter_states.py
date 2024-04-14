eeee0ae6e784fdd2ff4db1bd15848c95f38229d7
vale-hash <newlechim@gmail.com>
2024-04-14 20:34:45 +0100

    2-my filter done

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ def connection(username, password, db_name):
 
     curr = conn.cursor()
 
-    curr.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    curr.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
 
     query_rows = curr.fetchall()
 
