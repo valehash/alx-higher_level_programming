4ec68045e250f62892f04839372ccb4ff1e38b60
vale-hash <newlechim@gmail.com>
2024-04-14 08:35:32 +0100

    addded docs to 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
+"""A module that reads the states from a db"""
+
 import sys
 import MySQLdb
 
 def connection(username, password, db_name):
+    """The function that connects to the database and runs the query to retrieve states"""
+
     conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name, charset="utf8")
 
     curr = conn.cursor()
@@ -17,4 +21,7 @@ def connection(username, password, db_name):
     curr.close()
     conn.close()
 
-connection(sys.argv[1],sys.argv[2], sys.argv[3])
+if __name__ == '__main__':
+    connection(sys.argv[1],sys.argv[2], sys.argv[3])
+
+
