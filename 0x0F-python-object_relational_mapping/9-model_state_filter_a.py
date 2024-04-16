667956fa986abb31e23f6e0d81decb7199102ae6
vale-hash <newlechim@gmail.com>
2024-04-16 14:22:53 +0100

    Task 9 completed

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,28 @@
+"""
+Lists all State objects that contain the letter a from the database
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main(username, password, dbname):
+    db_url = f"""mysql+mysqldb://
+    {username}:{password}@localhost:3306
+    /{dbname}"""
+    engine = create_engine(db_url)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%')).all()
+
+    for rows in states:
+        print(f"{rows.id}: {rows.name}")
+    session.close()
+
+
+if __name__ == '__main__':
+    main(argv[1], argv[2], argv[3])
