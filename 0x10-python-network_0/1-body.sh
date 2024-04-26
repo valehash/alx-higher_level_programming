6b877ca72e28aefebb8d49e95326aa95cce2dc2f
vale-hash <newlechim@gmail.com>
2024-04-26 15:07:12 +0100

    wee

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #code to get the body if a webpage if the header response is 200
-[ $(curl -o /dev/null -s -w "%{http_code}" $1 ) -eq 200 ] && curl -s -L $1
+[ $(curl -o /dev/null -s -w -L "%{http_code}" $1 ) -eq 200 ] && curl -s $1
