fde4724fbf3254734ed2c433af0a9990894e1f35
vale-hash <newlechim@gmail.com>
2024-04-26 14:49:21 +0100

    3 lines long task 1 added redirection flag

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #code to get the body if a webpage if the header response is 200
-[ $(curl -o /dev/null -s -w "%{http_code}" $1 ) -eq 200 ] && curl -sI $1
+[ $(curl -o /dev/null -s -w "%{http_code}" $1 ) -eq 200 ] && curl -sI -L $1
