e11e813676823dc08fa32ff401ef5dec2bc7af6a
vale-hash <newlechim@gmail.com>
2024-05-24 01:35:01 +0100

    js api 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,22 +1,19 @@
 #!/usr/bin/node
-// code to get the title of starwars using the api
-const url = process.argv[2];
 
-const fs = require('node:fs')
+const { argv } = require('process');
+const request = require('request');
+const fs = require('node:fs');
 
-const req = require('request');
+const file = argv[3];
 
-const filename = 'loripsum';
-console.log(url);
-
-req(url, (error, response, body) => {
+request(argv[2], (error, response, body) => {
   if (error) {
     console.error(error);
   }
 
-  fs.writeFile(filename, body, err =>  {
-    if (err) {
-      console.error(err);
+  fs.writeFile(file, body, error => {
+    if (error) {
+      console.error(error);
     }
   });
 });
